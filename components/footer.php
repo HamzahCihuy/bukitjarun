3cd3d5146328ada4905ffc669d7208eb32284fe9
HamzahCihuy <237007076@student.unsil.ac.id>
2026-01-15 17:25:50 +0700

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -5,13 +5,6 @@ $footer = $pdo->query("SELECT * FROM footer_settings WHERE id=1")->fetch(PDO::FE
 ?>
 
 <footer class="relative bg-[#064E3B] text-white pt-20 pb-10 mt-20 font-fun">
-    
-    <div class="absolute top-0 left-0 w-full overflow-hidden leading-[0] transform -translate-y-full">
-        <svg class="relative block w-[calc(100%+1.3px)] h-[150px]" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
-                  class="fill-[#064E3B]"></path>
-        </svg>
-    </div>
 
     <div class="absolute inset-0 opacity-5 pointer-events-none" 
          style="background-image: url('https://www.transparenttextures.com/patterns/cubes.png');">
