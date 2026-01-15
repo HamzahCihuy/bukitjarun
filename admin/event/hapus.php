2557bd3f648e72ce88903b04b04ea65819fb4b4f
HamzahCihuy <237007076@student.unsil.ac.id>
2026-01-15 16:21:45 +0700

    Update hapus.php

--- admin/event/hapus.php
+++ admin/event/hapus.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true) {
+    header("Location: index.php");
+    exit;
+}
 include '../../db/koneksi.php';
 
 if (isset($_GET['id'])) {
