313eecfe19ec20b6f31180541db67d012b6dbc20
HamzahCihuy <237007076@student.unsil.ac.id>
2026-01-15 16:41:34 +0700

    Update index.php

--- admin/menfess/index.php
+++ admin/menfess/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true) {
-    header("Location: index.php");
+if (!isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true) {
+    header("Location: login.php");
     exit;
 }
 // 1. KONEKSI DATABASE
